e15403c3cd2570f0351519b55d08e6bad2ec69e1
josueegg <josueegg@outlook.com>
2025-06-20 18:20:20 +0000

    2° Etapa aventureiro.

--- Desafio_xadrez_primeira _etapa/Desafio_xadrez_primeira _etapa.c
+++ Desafio_xadrez_primeira _etapa/Desafio_xadrez_primeira _etapa.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
  int main(){
 
-    int  opcao;
+    int  opcao,movimento,movimentocompleto;
 
     printf("Selecione qual peça você deseja mexer.\n");
+    printf("1- Torre:\n");
+    printf("2- Bispo:\n");
+    printf("3- Rainha:\n");
+    printf("4- Cavalo:\n");
     scanf("%d", &opcao);
 
     switch (opcao)
@@ -31,10 +35,57 @@
         printf("Esquerda %d\n", rainha);
         rainha++;
       } while (rainha <= 8);
+      break;
+
+      case 4:
+      printf("Movimento com cavalo:\n");
+      printf("Escolha para qual lado deseja movimentar:\n");
+      printf("1-Cima Direita:\n"); 
+      printf("2-Cima Esquerda:\n");
+      printf("3-Baixo Direita:\n");
+      printf("4-Baixo Esquerda:\n");
+      scanf("%d", &movimento);
+
+        switch (movimento)
+         {
+         case 1:
+         printf("Movimento do cavalo:\n");
+     for (int cavalo = 1; cavalo < 3; cavalo++) {
+        printf("Cima:\n");
+     }printf("Direita:\n");
+     break;
+
+         case 2:
+         printf("Movimento do cavalo:\n");
+     for (int cavalo = 1; cavalo < 3; cavalo++) {
+        printf("Cima:\n");
+     }printf("Esquerda:\n");
+     break;
+
+         case 3:
+         printf("Movimento do cavalo:\n");
+     for (int cavalo = 1; cavalo < 3; cavalo++) {
+        printf("Baixo:\n");
+     }printf("Direita:\n");
+     break;
+
+         case 4:
+         printf("Movimento do cavalo:\n");
+     for (int cavalo = 1; cavalo < 3; cavalo++) {
+        printf("Baixo:\n");
+     }printf("Esquerda:\n");
+     break;
+            
+         default:
+         printf("Opção invalida!!");
+            break;
+         }
+      
+break;
 
      default:
-     printf("Opção inválida.\n");
-         
+     printf("Opção inválida!!\n");
+         break;
     }
 
 
